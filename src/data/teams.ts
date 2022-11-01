feff1d543a3312294908ad3dccf8f00e22bb295a
programwithjohn <john.cox0321@gmail.com>
2022-11-01 13:55:49 -0500

    yearly teams, to main branch

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,23 +1,59 @@
-import GitGudPhoto from "../assets/git-gud.jpg";
-import TeamONPhoto from "../assets/team-on.jpg";
-import DreamTeamPhoto from "../assets/dream-team.png";
-import TeamTuxPhoto from "../assets/team-tux.jpg";
+import GitGudPhoto2022 from "../assets/teams/2022/git-gud.jpg";
+import TeamONPhoto2022 from "../assets/teams/2022/team-on.jpg";
+import DreamTeamPhoto2022 from "../assets/teams/2022/dream-team.jpg";
+import TeamTuxPhoto2022 from "../assets/teams/2022/team-tux.jpg";
+import RuntimeTerrorPhoto2022 from "../assets/teams/2022/runtime-terror.jpg";
 
-export const teams = [
-    {
-        name: "Git Gud",
-        photo: GitGudPhoto
+import GitGudPhoto2021 from "../assets/teams/2021/git-gud.jpg";
+import TeamONPhoto2021 from "../assets/teams/2021/team-on.jpg";
+import DreamTeamPhoto2021 from "../assets/teams/2021/dream-team.png";
+import TeamTuxPhoto2021 from "../assets/teams/2021/team-tux.jpg";
+
+export const yearlyTeams = [
+    {   
+        year: 2022,
+        info: [
+            {
+                name: "Git Gud",
+                photo: GitGudPhoto2022
+            },
+            {
+                name: "Team O(N)",
+                photo: TeamONPhoto2022
+            },
+            {
+                name: "Dream Team",
+                photo: DreamTeamPhoto2022
+            },
+            {
+                name: "Team Tux",
+                photo: TeamTuxPhoto2022
+            },
+            {
+                name: "Runtime Terror",
+                photo: RuntimeTerrorPhoto2022
+            }
+        ] 
     },
-    {
-        name: "Team O(n)",
-        photo: TeamONPhoto
-    },
-    {
-        name: "Dream Team",
-        photo: DreamTeamPhoto
-    },
-    {
-        name: "Team Tux",
-        photo: TeamTuxPhoto
+    {   
+        year: 2021,
+        info: [
+            {
+                name: "Git Gud",
+                photo: GitGudPhoto2021
+            },
+            {
+                name: "Team O(N)",
+                photo: TeamONPhoto2021
+            },
+            {
+                name: "Dream Team",
+                photo: DreamTeamPhoto2021
+            },
+            {
+                name: "Team Tux",
+                photo: TeamTuxPhoto2021
+            },
+        ]
     }
 ];
